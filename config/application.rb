12816c8f0aaff0a484a267ab16a7a13eaad38b97
Ian Jones <jones58ian@gmail.com>
2016-07-06 19:44:46 +0000

    fix fonts

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module Nomster
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Precompile additional assets
-    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    config.assets.precompile += %w( .svg .eot .woff .ttf .woff2)
   end
 end
