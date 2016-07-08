380c8ffb8c801a64f36473db94fdd836d01dca0d
Ian Jones <jones58ian@gmail.com>
2016-07-08 19:11:49 +0000

    add validation for decription, address, and add max and min length for name

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 140, minimum: 3}
+  validates :address, :description, presence: true
   self.per_page = 5
 end
